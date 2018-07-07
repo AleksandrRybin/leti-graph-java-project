572cceae515e4741d387c323c0b4bc7249984a14
AleksandrRybin <Vfibyrf007@gmail.com>
2018-07-07 14:49:55 +0300

    fix checker

--- src/business_logic/Checker.java
+++ src/business_logic/Checker.java
@@ -2,38 +2,42 @@ package business_logic;
 
 public class Checker {
     private final int[][] indMatrix;
-    private final boolean[] visited;
+    private int[] visited;
 
     public Checker(Graph graph) {
         this.indMatrix = graph.getIndMatrix();
-        visited = new boolean[indMatrix.length];
+        visited = new int[indMatrix.length];
+
+        for (int i = 0; i < visited.length; i++) {
+            visited[i] = DFSSTATE.WHITE;
+        }
     }
 
     private boolean dfs(int u) {
-        visited[u] = true;
+        visited[u] = DFSState.GREY;
 
         for (int v = 0; v < indMatrix[u].length; v++) {
             if (indMatrix[u][v] != 0) {
-                if (visited[v] == false) {
+                if (visited[v] == DFSSTATE.WHITE) {
                     boolean isCycleByNextVertex = dfs(v);
 
                     if (isCycleByNextVertex){
                         return true;
                     }
-
                 }
-                else {
+                else if (visited[v] == DFSSTATE.GREY){
                     return true;
                 }
             }
         }
 
+        visited[u] == DFSState.BLACK;
         return false;
     }
 
     public boolean isCycle() {
         for (int u = 0; u < indMatrix.length; u++) {
-            if (visited[u] == false) {
+            if (visited[u] == DFSSTATE.WHITE) {
                 if (dfs(u)) {
                     return true;
                 }
@@ -41,5 +45,11 @@ public class Checker {
         }
 
         return false;
+    }
+
+    private enum DFSState {
+        WHITE,
+        GREY,
+        BLACK
     }
 }
